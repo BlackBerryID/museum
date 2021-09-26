f2c802a2eb86a7c1ab0ce5ede671c1e01555c139
BlackBerryID <iddf1994one@gmail.com>
2021-09-26 21:15:33 +0300

    refactor: delete old versions of files

--- museum/src/js/progress-bar.js
+++
@@ -1,8 +0,0 @@
-const progressBar = document.querySelectorAll(".input");
-
-progressBar.forEach((item) =>
-  item.addEventListener("input", function () {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, white 100%)`;
-  })
-);
