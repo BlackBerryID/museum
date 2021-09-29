cdbf0be0c41c248d169aff4a13ec036e9f16eb97
BlackBerryID <iddf1994one@gmail.com>
2021-09-29 19:55:14 +0300

    fix: close algorithm of burger menu and position of popup close button

--- museum-adaptive/src/js/burger-menu.js
+++ museum-adaptive/src/js/burger-menu.js
@@ -2,6 +2,7 @@ const header = document.querySelector("header");
 const menuBtn = document.querySelector(".menuBtn");
 const body = document.querySelector("body");
 const headerNav = document.querySelector(".header-nav");
+const navLinks = document.querySelectorAll(".nav-link-top");
 
 function toggleMenu() {
   if (!header.classList.contains("menuOpen")) {
@@ -16,3 +17,17 @@ function toggleMenu() {
 }
 
 menuBtn.addEventListener("click", toggleMenu);
+navLinks.forEach((item) =>
+  item.addEventListener("click", () =>
+    window.innerWidth <= 1024 ? toggleMenu() : ""
+  )
+);
+document.addEventListener("click", (e) => {
+  if (
+    e.target != headerNav &&
+    body.classList.contains("menuOpen") &&
+    e.target != menuBtn
+  ) {
+    toggleMenu();
+  }
+});
