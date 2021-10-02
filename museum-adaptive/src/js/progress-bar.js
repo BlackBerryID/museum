0be8f3f45266311aaa0e4624cbecdb2d91a3059f
BlackBerryID <iddf1994one@gmail.com>
2021-10-02 20:10:25 +0300

    fix: look through all adaptations. Make a few corrections

--- museum-adaptive/src/js/progress-bar.js
+++ museum-adaptive/src/js/progress-bar.js
@@ -6,3 +6,20 @@ progressBar.forEach((item) =>
     this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, white 100%)`;
   })
 );
+
+const inputProgress = document.querySelector(".input-progress");
+const inputVolume = document.querySelector(".input-volume");
+
+function setRangeInputPos() {
+  if (window.innerWidth > 1024) {
+    inputProgress.setAttribute("value", "54");
+    console.log(window.innerWidth);
+  } else if (window.innerWidth > 768) {
+    inputProgress.setAttribute("value", "40");
+  } else {
+    inputProgress.setAttribute("value", "31");
+  }
+}
+
+setRangeInputPos();
+window.addEventListener("resize", setRangeInputPos);
