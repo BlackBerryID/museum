8150471794a50d8ea448aa18767263110cc65e20
BlackBerryID <iddf1994one@gmail.com>
2021-10-09 18:09:18 +0300

    feat: add sessionStorage support in tickets section

--- museum-dom/src/js/section-tickets.js
+++ museum-dom/src/js/section-tickets.js
@@ -17,10 +17,13 @@ labelList.forEach((item) =>
       e.target.classList.add("active");
       if (e.target.classList.contains("permanent")) {
         ticketTypeCost = 20;
+        sessionStorage.setItem("ticketType", "permanent");
       } else if (e.target.classList.contains("temporary")) {
         ticketTypeCost = 25;
+        sessionStorage.setItem("ticketType", "temporary");
       } else if (e.target.classList.contains("combined")) {
         ticketTypeCost = 40;
+        sessionStorage.setItem("ticketType", "combined");
       }
       priceRecount();
     }
@@ -28,13 +31,39 @@ labelList.forEach((item) =>
 );
 
 ticketsInputButtonList.forEach((item) =>
-  item.addEventListener("click", priceRecount)
+  item.addEventListener("click", () => {
+    priceRecount();
+    sessionStorage.setItem(
+      "basicTicketsInputValue",
+      `${basicTicketsInput.value}`
+    );
+    sessionStorage.setItem(
+      "seniorTicketsInputValue",
+      `${seniorTicketsInput.value}`
+    );
+  })
 );
 
 function priceRecount() {
   totalPrice.textContent =
     ticketTypeCost * basicTicketsInput.value +
     (ticketTypeCost / 2) * seniorTicketsInput.value;
+}
+
+const basicTicketsInputValue = sessionStorage.getItem("basicTicketsInputValue");
+const seniorTicketsInputValue = sessionStorage.getItem(
+  "seniorTicketsInputValue"
+);
+const ticketTypeValue = sessionStorage.getItem("ticketType");
+
+if (basicTicketsInputValue) basicTicketsInput.value = basicTicketsInputValue;
+if (seniorTicketsInputValue) seniorTicketsInput.value = seniorTicketsInputValue;
+if (ticketTypeValue) {
+  labelList.forEach((item) => {
+    item.classList.remove("active");
+    if (item.classList.contains(sessionStorage.getItem("ticketType")))
+      item.classList.add("active");
+  });
 }
 
 priceRecount();
